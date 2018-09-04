37b4fe20c21ed35f9f493be30374e5796117d636
Philip Vejre <philipvejre@gmail.com>
2018-09-04 14:40:21 +0000

    Added instructive comments

--- example/trace_it.py
+++ example/trace_it.py
@@ -2,6 +2,7 @@
 
 from deadpool_dca import *
 
+# Choose different types of data to trace here. More information at https://github.com/SideChannelMarvels/Deadpool
 filters = [
     # Filter('data_R', ['R'], lambda stack_range, addr, size, data: size <= 4, lambda addr, size, data: data & 0xFF, '<B'),
     Filter('data_W', ['W'], lambda stack_range, addr, size, data: size <= 4, lambda addr, size, data: data & 0xFF, '<B'),
@@ -11,11 +12,13 @@ filters = [
     # Filter('addr_RW', ['R','R'], lambda stack_range, addr, size, data: size <= 4, lambda addr, size, data: addr & 0xFF, '<B')
 ]
 
+# Choose binary and address range
 T=TracerGrind('./aes_simple',
               filters=filters,
               arch=ARCH.amd64,
               addr_range='0x109080-0x109785')
 
+# Choose number of traces to collect
 T.run(100)
 bin2daredevil(keywords=filters,
               configs={'attack_sbox':    {'algorithm':'AES', 'position':'LUT/AES_AFTER_SBOX'}})
